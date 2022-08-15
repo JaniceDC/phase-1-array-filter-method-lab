68b0218c3323f3c23a473e449a9d032cec8141e6
JaniceDC <janice.dipayan.chen@gmail.com>
2022-08-15 13:23:15 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const findMatching = (drivers, string) => {
+    const driverName = drivers.filter((result) => {
+        return result.toUpperCase() === string.toUpperCase();
+
+    });
+    return driverName;
+}
+
+const fuzzyMatch = (drivers, string) => {
+    const driverName = drivers.filter((result) => {
+        return result[0] === string[0];
+    });
+    return driverName;
+}
+
+const matchName = (drivers, string) => {
+    const driverName = drivers.filter((result) => {
+        return result.name === string;
+    });
+    return driverName
+}
